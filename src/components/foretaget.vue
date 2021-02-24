6167a58512ef5b61a8a8e6ed04442f08f7576ecf
FredrikGehrke <57892335+FredrikGehrke@users.noreply.github.com>
2021-02-24 09:47:43 +0100

    fixes

--- src/components/foretaget.vue
+++ src/components/foretaget.vue
@@ -1,20 +1,21 @@
 <template>
     <div class="py-5 z-depth-1 bg-color">
-        <section class="white-text container animated fadeIn ">
+        <section class="white-text container animated fadeIn">
             <div class="row pr-lg-5">
-                <div class="col-md-7 mb-4">
+                <div class="col-md-6 mb-4 my-auto">
 
-                  <div class="col-md-11 animated">
-                      <!-- Google Maps -->
-                      <div id="map-within-card-2" class="map-container rounded-left rounded-right z-depth-5 wow fadeIn" style="height: 320px">
-                          <iframe src="https://maps.google.com/maps?q=Legeringsgatan6&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
-                          style="border:0" allowfullscreen></iframe>
-                      </div>
-                      <!-- Google Maps -->
+                  <div class="col-md-11">
+                    <div id="map-within-card-2" class="map-container rounded-left rounded-right z-depth-5 wow fadeIn" style="height: 320px">
+                        <iframe src="https://maps.google.com/maps?q=Legeringsgatan6&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
+                        style="border:0" allowfullscreen></iframe>
+                    </div>
                   </div>
+                  <!-- <div class="justify-content-center d-flex my-auto">
+                    <img src="@/assets/irl-image.jpg" class="rounded" alt="...">
+                  </div> -->
 
                 </div>
-                <div class="col-md-5 d-flex align-items-center">
+                <div class="col-md-6 d-flex align-items-center">
                     <div>
                     
                     <h1 class="font-weight-bold mb-2 theme-color wow fadeIn" style="animation-delay: 400ms;">Om företaget</h1>
@@ -22,9 +23,11 @@
                       <p class="mb-4">Vi startade år 2000 och är två ingenjörer.</p>
 
                       <p>Vi finns i Teknikbyn i Västerås. Våra kunder är energiproducenter och anläggningsleverantörer, främst i Mälardalen.</p>
+                      <p>Vårt kompetensområde innefattar mekanisk konstruktion, hållfasthetsberäkning av trycksatta rör och komponenter samt spänningsanalys av rörsystem. </p>
+                      <p>Vi levererar komplett underlag för CE-märkning enligt <strong class="font-weight-bold">PED 97/23/EG</strong>.</p>
                       <p class="font-weight-bold">Vi tar gärna ansvar för upphandling och projektledning i kundens projekt.</p>
 
-                      <router-link to="/kontakta-oss" class="btn theme-bg animated fadeIn ml-0" style="animation-delay: 900ms;">KONTAKT</router-link>
+                      <!-- <router-link to="/kontakta-oss" class="btn theme-bg animated fadeIn ml-0" style="animation-delay: 900ms;">KONTAKT</router-link> -->
                     </div>
 
 
